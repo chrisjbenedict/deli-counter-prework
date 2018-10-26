daac9ff080325339c6b130e875df585f19cc6563
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:55:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,10 @@ def line(arr)
     puts "The line is currently empty."
   end
   line_number = 1
+  current_line = []
   for person in arr
-    puts "The line is currently: #{line_number}. #{person}"
+    current_line.push("The line is currently: #{line_number}. #{person}")
     line_number += 1
   end
+  return current_line
 end
