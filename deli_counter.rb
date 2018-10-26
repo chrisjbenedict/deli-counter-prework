5bd7cf799b870172e293c89ec6459e4d32ed5a92
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:50:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,15 @@
 # Write your code here.
 katz_deli = []
 
-def line(line)
-  if line.size == 0
+def line(arr)
+  if arr.size == 0
     puts "The line is currently empty."
   end
   current_line = []
-  for person in line
-    current_line.push("#{line[person]}. #{person}")
+  line_number = 1
+  arr.each do |person|
+    current_line.push("#{line_number}. #{person}")
+    line_number += 1
   end
   return current_line
 end
