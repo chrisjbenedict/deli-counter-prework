3fc9e7f2341ec8e18864a570811883b5ba9fa322
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:57:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,14 @@ katz_deli = []
 def line(arr)
   if arr.size == 0
     puts "The line is currently empty."
+  elsif arr.size > 0
+    line_number = 1
+    current_line = []
+    intro = "The line is currently: "
+    for person in arr
+      current_line.push("#{line_number}. #{person}")
+      line_number += 1
+    end
+    puts intro + current_line.join(' ')
   end
-  line_number = 1
-  current_line = []
-  intro = "The line is currently: "
-  for person in arr
-    current_line.push("#{line_number}. #{person}")
-    line_number += 1
-  end
-  puts intro + current_line.join(' ')
 end
