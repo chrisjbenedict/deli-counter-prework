652c5f11a1be2ca9b71826ce986a7bcfb5221b35
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 21:11:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
-  elsif katz_deli.size > 0
+  else
     line_number = 1
     current_line = []
     intro = "The line is currently: "
