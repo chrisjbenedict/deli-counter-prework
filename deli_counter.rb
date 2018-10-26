e40bb7276d81a19f9b695c9598422247c793557c
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:48:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  end
+  current_line = []
+  for person in line
+    current_line.push("#{line[person]}. #{person}")
+  end
+  return current_line
+end
